5232f9452954ca1db56c0cc44a79897ab0e5ccde
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2026-03-21 17:44:01 -0600

    fix(cors) added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { startServer } from './services/mongo.js';
 import membersRouter from './enpoints/members.js';
 
 const app = express();
 startServer(app);
 
+app.use(cors({
+	origin: [
+		'http://localhost:5173',
+		'https://kinasisdev.shop',
+		'https://www.kinasisdev.shop'
+	]
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {res.send('Welcome to Kinasis API!');});
