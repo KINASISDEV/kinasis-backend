4ec834ec0e8a2cc5d2b175f5beefe03ef0520816
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2026-04-02 15:56:25 -0600

    feat(model): set new and necesary attributes

--- src/models/servicesModel.js
+++ src/models/servicesModel.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 export const servicesSchema = new mongoose.Schema({
     uuid: String,
+    createAt: Date,
     active: Boolean,
     stellar: Boolean,
     tittle: String,
     description: String,
     components: [String],
+    iconRoute: String,
 });
